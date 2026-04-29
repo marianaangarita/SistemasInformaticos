fdd0974b0f263801f2de3bbe95564461c7f46899
marianaangarita <mariana.angarita.afonso@gmail.com>
2026-04-29 16:57:57 +0200

    refactor: remove Flask application and product database files

--- database.py
+++
@@ -1,5 +0,0 @@
-productos = [
-    { 'id': 'A001', 'titulo': 'Monitor 27', 'precio': 300 },
-    { 'id': 'A002', 'titulo': 'CPU-Zi', 'precio': 100 },
-    { 'id': 'A003', 'titulo': 'Microfono', 'precio': 24 }
-]
